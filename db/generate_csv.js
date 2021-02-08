18bc2a84a6695bf27f66e9762c845c906ae94fd6
7MIMIRA <7mimira@gmail.com>
2021-02-07 23:56:37 -0500

    Implement DB data generation, and import files / scripts

---
+++ db/generate_csv.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const generator = require('./generateSellers.js')
+
+const CSV_SIZE = 10_000_000;
+const DATA_CHUNK_SIZE = 1_000;
+
+let writeStream = fs.createWriteStream(path.join(__dirname, 'mongoDB_sellers.csv'));
+let csvHeader = generator.header();
+writeStream.write(csvHeader, 'utf-8');
+for (let id = 1; id < CSV_SIZE; id += DATA_CHUNK_SIZE) {
+  let sellers = generator.addSellers(id, DATA_CHUNK_SIZE);
+  writeStream.write(sellers, 'utf-8');
+}
+
+writeStream.end();
--- db/index.js
+++ db/index.js
@@ -1,10 +1,51 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: 'joselopez',
-  host: 'localhost',
-  database: 'seller',
-  password: '',
-  port: 5432
-})
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+mongoose.connect('mongodb://localhost/fetsy', {useNewUrlParser: true, useUnifiedTopology: true});
 
-module.exports = pool;
+const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('we are connected!');
+// });
+
+
+
+const sellerSchema = new Schema({
+  seller_rating: Number,
+  total_sales: Number,
+  seller_name: String,
+  seller_city: String,
+  seller_state: String,
+  on_etsy_since: Number,
+  item_id: Number
+  // item_id: {
+  //   type: Number,
+  //   index: true,
+  //   unique: true
+  // }
+});
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
+// let seller = new Seller({
+//   seller_rating: 5,
+//   total_sales: 1024,
+//   seller_name: 'Jose',
+//   seller_city: 'Cityville',
+//   seller_state: 'Colorado',
+//   on_etsy_since: 1995,
+//   item_id: 2
+// });
+
+// seller.save()
+//   .then(() => {
+//     db.close();
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+  module.exports = {
+    Seller: Seller,
+    Connection: db
+  };
