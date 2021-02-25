02f210a79cd167c65f25e67a433641e76e629514
7MIMIRA <7mimira@gmail.com>
2021-02-25 01:23:33 -0500

    Add k6 server stress test scripts

---
+++ tests/stress-testing/k6script10_post.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+
+export let options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 10,
+      timeUnit: '1s',
+      duration: '1m',
+      preAllocatedVUs: 20,
+      maxVUs: 200
+    }
+  }
+};
+
+const url = 'http://localhost:3005/items/11/seller';
+
+export default function () {
+  let data = {
+    seller_rating: 1,
+    total_sales: 100,
+    seller_name: 'Grezzzy Reeeeeeeeeed',
+    seller_city: 'Dos S antos',
+    seller_state: 'Calinunya',
+    on_etsy_since: 2020
+  };
+  http.post(url, data);
+}
