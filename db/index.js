a435c7c5d08a6d716c890d8d1c4a2cfd46ec879b
7MIMIRA <7mimira@gmail.com>
2021-03-06 22:49:22 -0500

    Add environment variables for local development

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'joselopez',
-  host: 'localhost',
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || '3.141.168.86',
   database: 'seller',
   password: '',
   port: 5432
