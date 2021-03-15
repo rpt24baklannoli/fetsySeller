4338f5c79962a9e5739e563a86cd10d05c36c29d
7MIMIRA <7mimira@gmail.com>
2021-03-14 23:36:39 -0400

    update host ip address

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || '3.141.168.86',
+  host: process.env.DB_HOST || '3.17.207.149',
   database: 'seller',
   password: process.env.DB_PASSWORD || 'student',
   port: 5432
